afec1aff1d26c634779c69e83a736ec4e88ec3ec
Anthony Twesigye <twesigye10@gmail.com>
2021-11-02 11:09:49 +0300

    loaded data

--- R/checks_for_data_collection.R
+++ R/checks_for_data_collection.R
@@ -3,11 +3,16 @@ library(lubridate)
 library(glue)
 source("R/checks_for_other_responses.R")
 
+# read data 
+df_tool_data <- readxl::read_excel("inputs/PA_KI_test_data.xlsx") %>% 
+  mutate(i.check.uuid = `_uuid`,
+         i.check.start_date = as_date(start),
+         i.check.progres_id = progres_id,
+         i.check.location = location
+         )
+
+df_survey <- readxl::read_excel("inputs/PA_KI_questionnaire_Tool_2021.xlsx", sheet = "survey")
+df_choices <- readxl::read_excel("inputs/PA_KI_questionnaire_Tool_2021.xlsx", sheet = "choices")
 
 
-# time of survey
-
-# duplicates
-
-# logical checks to be identified
 
