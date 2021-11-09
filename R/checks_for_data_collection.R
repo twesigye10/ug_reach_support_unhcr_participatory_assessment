6194b0f44c91ee202265493dc5028f8e00df0e12
Anthony Twesigye <twesigye10@gmail.com>
2021-11-09 16:55:41 +0300

    key_stakeholders

--- R/checks_for_data_collection.R
+++ R/checks_for_data_collection.R
@@ -18,10 +18,19 @@ check_quantitative_tool_data(input_tool_data_name = "PA_KI_questionnaire_October
 
 # qualitative data -------------------------------------------------------
 
-# PA_KI_host_community_questionnaire_October_2021
+# host_community ----------------------------------------------------------
+
 check_qualitative_tool_data(input_tool_data_name = "PA_KI_host_community_questionnaire_October_2021",
                             input_tool_name = "PA_KI_host_community_Tool_2021")
 
-# PA_KI_host_community_questionnaire_October_2021_Kampala
 check_qualitative_tool_data(input_tool_data_name = "PA_KI_host_community_questionnaire_October_2021_Kampala",
                             input_tool_name = "PA_KI_host_community_Tool_2021_Kampala")
+
+# key_stakeholders --------------------------------------------------------
+
+
+check_qualitative_tool_data(input_tool_data_name = "PA_KI_key_stakeholders_questionnaire_October_2021",
+                            input_tool_name = "PA_KI_key_stakeholders_Tool_2021")
+
+check_qualitative_tool_data(input_tool_data_name = "PA_KI_key_stakeholders_questionnaire_October_2021_Kampala",
+                            input_tool_name = "PA_KI_key_stakeholders_Tool_2021_Kampala")
