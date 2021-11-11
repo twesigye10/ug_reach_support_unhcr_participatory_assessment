14f27296e666f347dfed9b216e6eb20374b8c70e
Anthony Twesigye <twesigye10@gmail.com>
2021-11-11 11:30:22 +0300

    renamed file

---
+++ R/cleaning_log_implementation.R
@@ -0,0 +1,51 @@
+library(tidyverse)
+library(lubridate)
+library(glue)
+source("R/cleaning_log_function.R")
+
+# quantitative data -------------------------------------------------------
+
+# PA_KI_questionnaire_September_2021
+cleaning_log(input_tool_data_name = "PA_KI_questionnaire_September_2021",
+                             input_tool_name = "PA_KI_questionnaire_Tool_2021")
+
+# PA_KI_questionnaire_October_2021_Kampala
+cleaning_log(input_tool_data_name = "PA_KI_questionnaire_October_2021_Kampala",
+                             input_tool_name = "PA_KI_questionnaire_Tool_2021_Kampala")
+
+# qualitative data -------------------------------------------------------
+
+# host_community ----------------------------------------------------------
+
+cleaning_log(input_tool_data_name = "PA_KI_host_community_questionnaire_October_2021",
+                            input_tool_name = "PA_KI_host_community_Tool_2021")
+
+cleaning_log(input_tool_data_name = "PA_KI_host_community_questionnaire_October_2021_Kampala",
+                            input_tool_name = "PA_KI_host_community_Tool_2021_Kampala")
+
+# key_stakeholders --------------------------------------------------------
+
+
+cleaning_log(input_tool_data_name = "PA_KI_key_stakeholders_questionnaire_October_2021",
+                            input_tool_name = "PA_KI_key_stakeholders_Tool_2021")
+
+cleaning_log(input_tool_data_name = "PA_KI_key_stakeholders_questionnaire_October_2021_Kampala",
+                            input_tool_name = "PA_KI_key_stakeholders_Tool_2021_Kampala")
+
+# specific_groups --------------------------------------------------------
+
+
+cleaning_log(input_tool_data_name = "PA_KI_specific_groups_questionnaire_October_2021",
+                            input_tool_name = "PA_KI_specific_groups_Tool_2021")
+
+cleaning_log(input_tool_data_name = "PA_KI_specific_groups_questionnaire_October_2021_Kampala",
+                            input_tool_name = "PA_KI_specific_groups_Tool_2021_Kampala")
+
+# other_minorities --------------------------------------------------------
+
+
+cleaning_log(input_tool_data_name = "PA_KI_other_minorities_questionnaire_October_2021",
+                            input_tool_name = "PA_KI_other_minorities_Tool_2021")
+
+cleaning_log(input_tool_data_name = "PA_KI_other_minorities_questionnaire_October_2021_Kampala",
+                            input_tool_name = "PA_KI_other_minorities_Tool_2021_Kampala")
