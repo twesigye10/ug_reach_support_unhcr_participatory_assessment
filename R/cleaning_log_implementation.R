f5dafccc65b781707a9b384b6844854e7f89ee3e
Anthony Twesigye <twesigye10@gmail.com>
2021-11-11 11:39:22 +0300

    code refactor

--- R/cleaning_log_implementation.R
+++ R/cleaning_log_implementation.R
@@ -16,7 +16,6 @@ cleaning_log(input_log_name = "PA_KI_questionnaire_October_2021_Kampala_combined
 # qualitative data -------------------------------------------------------
 
 # host_community ----------------------------------------------------------
-
 cleaning_log(input_log_name = "PA_KI_host_community_questionnaire_October_2021_combined_checks",
              input_tool_data_name = "PA_KI_host_community_questionnaire_October_2021",
                             input_tool_name = "PA_KI_host_community_Tool_2021")
@@ -26,8 +25,6 @@ cleaning_log(input_log_name = "PA_KI_host_community_questionnaire_October_2021_K
                             input_tool_name = "PA_KI_host_community_Tool_2021_Kampala")
 
 # key_stakeholders --------------------------------------------------------
-
-
 cleaning_log(input_log_name = "PA_KI_key_stakeholders_questionnaire_October_2021_combined_checks",
              input_tool_data_name = "PA_KI_key_stakeholders_questionnaire_October_2021",
                             input_tool_name = "PA_KI_key_stakeholders_Tool_2021")
@@ -37,8 +34,6 @@ cleaning_log(input_log_name = "PA_KI_key_stakeholders_questionnaire_October_2021
                             input_tool_name = "PA_KI_key_stakeholders_Tool_2021_Kampala")
 
 # specific_groups --------------------------------------------------------
-
-
 cleaning_log(input_log_name = "PA_KI_specific_groups_questionnaire_October_2021_combined_checks",
              input_tool_data_name = "PA_KI_specific_groups_questionnaire_October_2021",
                             input_tool_name = "PA_KI_specific_groups_Tool_2021")
@@ -48,8 +43,6 @@ cleaning_log(input_log_name = "PA_KI_specific_groups_questionnaire_October_2021_
                             input_tool_name = "PA_KI_specific_groups_Tool_2021_Kampala")
 
 # other_minorities --------------------------------------------------------
-
-
 cleaning_log(input_log_name = "PA_KI_other_minorities_questionnaire_October_2021_combined_checks",
              input_tool_data_name = "PA_KI_other_minorities_questionnaire_October_2021",
                             input_tool_name = "PA_KI_other_minorities_Tool_2021")
