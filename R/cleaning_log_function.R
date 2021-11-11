7ed4985b71ab4174316d2d74201f67c2ad2a2595
Anthony Twesigye <twesigye10@gmail.com>
2021-11-11 11:38:08 +0300

    convert cleaning steps into a function

--- R/cleaning_log_function.R
+++ R/cleaning_log_function.R
@@ -1,79 +1,79 @@
 # using the cleaning log to clean the data
 
-library(tidyverse)
-library(lubridate)
-
-# read data
-df_cleaning_log <- read_csv("inputs/combined_logic_spatial_and_others_checks.csv") %>% 
-  mutate(adjust_log = ifelse(is.na(adjust_log), "apply_suggested_change", adjust_log)) %>%
-  filter(adjust_log != "delete_log", !is.na(value), !is.na(uuid)) %>% 
-  mutate(sheet = NA, index = NA, relevant = NA) %>% 
-  select(uuid, type, name, value, issue_id, sheet, index, relevant, issue)
-
-df_raw_data <- readxl::read_excel(path = "inputs/UGA2103_Financial_Service_Providers_Assessment_HH_Tool_June2021.xlsx", col_types = c_types) %>% 
-  filter(consent == "yes")
-
-df_survey <- readxl::read_excel("inputs/UGA2103_Digital_Finace_HH_Tool_June2021.xlsx", sheet = "survey") 
-df_choices <- readxl::read_excel("inputs/UGA2103_Digital_Finace_HH_Tool_June2021.xlsx", sheet = "choices") 
-
-# find all new choices to add to choices sheet ----------------------------
-
-# gather choice options based on unique choices list
-df_grouped_choices<- df_choices %>% 
-  group_by(list_name) %>% 
-  summarise(choice_options = paste(name, collapse = " : "))
-# get new name and ad_option pairs to add to the choices sheet
-new_vars <- df_cleaning_log %>% 
-  filter(type %in% c("change_response", "add_option")) %>% 
-  left_join(df_survey, by = "name") %>% 
-  filter(str_detect(string = type.y, pattern = "select_one|select one|select_multiple|select multiple")) %>% 
-  separate(col = type.y, into = c("select_type", "list_name"), sep =" ", remove = TRUE, extra = "drop") %>% 
-  left_join(df_grouped_choices, by = "list_name") %>%
-  filter(!str_detect(string = choice_options, pattern = value ) ) %>%
-  rename(choice = value ) %>%
-  select(name, choice) %>%
-  distinct() %>% # to make sure there are no duplicates
-  arrange(name)
-
-# create kobold object ----------------------------------------------------
-
-kbo <- kobold::kobold(survey = df_survey, 
-                      choices = df_choices, 
-                      data = df_raw_data, 
-                      cleaning = df_cleaning_log)
-
-# modified choices for the survey tool --------------------------------------
-df_choises_modified <- butteR:::xlsform_add_choices(kobold = kbo, new_choices = new_vars)
-
-# special treat for variables for select_multiple, we need to add the columns to the data itself
-df_survey_sm <- df_survey %>% 
-  mutate(q_type = case_when(str_detect(string = type, pattern = "select_multiple|select multiple") ~ "sm",
-                            str_detect(string = type, pattern = "select_one|select one") ~ "so",
-                            TRUE ~ type)) %>% 
-  select(name, q_type)
-# construct new columns for select multiple
-new_vars_sm <- new_vars %>% 
-  left_join(df_survey_sm, by = "name") %>% 
-  filter(q_type == "sm") %>% 
-  mutate(new_cols = paste0(name,"/",choice))
-
-# add new columns to the raw data -----------------------------------------
-
-df_raw_data_modified <- df_raw_data %>% 
-  butteR:::mutate_batch(nm = new_vars_sm$new_cols, value = F )
-
-# make some cleanup -------------------------------------------------------
-kbo_modified <- kobold::kobold(survey = df_survey %>% filter(name %in% colnames(df_raw_data_modified)), 
-                               choices = df_choises_modified, 
-                               data = df_raw_data_modified, 
-                               cleaning = df_cleaning_log )
-kbo_cleaned <- kobold::kobold_cleaner(kbo_modified)
-
-# handling added responses after starting data collection -----------------
-
-df_final_cleaned_data <- kbo_cleaned$data %>% 
-  mutate(across(.cols = contains("/"), .fns = ~ifelse(is.na(.), FALSE, .)))
-
-# write final modified data -----------------------------------------------------
-
-write_csv(df_final_cleaned_data, file = paste0("outputs/", butteR::date_file_prefix(), "_clean_data.csv"))
+cleaning_log <- function(input_log_name, input_tool_data_name, input_tool_name) {
+  # read data
+  df_cleaning_log <- read_csv(paste0("inputs/", input_log_name, ".csv")) %>% 
+    mutate(adjust_log = ifelse(is.na(adjust_log), "apply_suggested_change", adjust_log)) %>%
+    filter(adjust_log != "delete_log", !is.na(value), !is.na(uuid)) %>% 
+    mutate(sheet = NA, index = NA, relevant = NA) %>% 
+    select(uuid, type, name, value, issue_id, sheet, index, relevant, issue)
+  
+  df_raw_data <- readxl::read_excel(path = paste0("inputs/", input_tool_data_name, ".xlsx"), col_types = c_types) %>% 
+    filter(consent == "yes")
+  
+  df_survey <- readxl::read_excel(paste0("inputs/", input_tool_name, ".xlsx"), sheet = "survey") 
+  df_choices <- readxl::read_excel(paste0("inputs/", input_tool_name, ".xlsx"), sheet = "choices") 
+  
+  # find all new choices to add to choices sheet ----------------------------
+  
+  # gather choice options based on unique choices list
+  df_grouped_choices<- df_choices %>% 
+    group_by(list_name) %>% 
+    summarise(choice_options = paste(name, collapse = " : "))
+  # get new name and ad_option pairs to add to the choices sheet
+  new_vars <- df_cleaning_log %>% 
+    filter(type %in% c("change_response", "add_option")) %>% 
+    left_join(df_survey, by = "name") %>% 
+    filter(str_detect(string = type.y, pattern = "select_one|select one|select_multiple|select multiple")) %>% 
+    separate(col = type.y, into = c("select_type", "list_name"), sep =" ", remove = TRUE, extra = "drop") %>% 
+    left_join(df_grouped_choices, by = "list_name") %>%
+    filter(!str_detect(string = choice_options, pattern = value ) ) %>%
+    rename(choice = value ) %>%
+    select(name, choice) %>%
+    distinct() %>% # to make sure there are no duplicates
+    arrange(name)
+  
+  # create kobold object ----------------------------------------------------
+  
+  kbo <- kobold::kobold(survey = df_survey, 
+                        choices = df_choices, 
+                        data = df_raw_data, 
+                        cleaning = df_cleaning_log)
+  
+  # modified choices for the survey tool --------------------------------------
+  df_choises_modified <- butteR:::xlsform_add_choices(kobold = kbo, new_choices = new_vars)
+  
+  # special treat for variables for select_multiple, we need to add the columns to the data itself
+  df_survey_sm <- df_survey %>% 
+    mutate(q_type = case_when(str_detect(string = type, pattern = "select_multiple|select multiple") ~ "sm",
+                              str_detect(string = type, pattern = "select_one|select one") ~ "so",
+                              TRUE ~ type)) %>% 
+    select(name, q_type)
+  # construct new columns for select multiple
+  new_vars_sm <- new_vars %>% 
+    left_join(df_survey_sm, by = "name") %>% 
+    filter(q_type == "sm") %>% 
+    mutate(new_cols = paste0(name,"/",choice))
+  
+  # add new columns to the raw data -----------------------------------------
+  
+  df_raw_data_modified <- df_raw_data %>% 
+    butteR:::mutate_batch(nm = new_vars_sm$new_cols, value = F )
+  
+  # make some cleanup -------------------------------------------------------
+  kbo_modified <- kobold::kobold(survey = df_survey %>% filter(name %in% colnames(df_raw_data_modified)), 
+                                 choices = df_choises_modified, 
+                                 data = df_raw_data_modified, 
+                                 cleaning = df_cleaning_log )
+  kbo_cleaned <- kobold::kobold_cleaner(kbo_modified)
+  
+  # handling added responses after starting data collection -----------------
+  
+  df_final_cleaned_data <- kbo_cleaned$data %>% 
+    mutate(across(.cols = contains("/"), .fns = ~ifelse(is.na(.), FALSE, .)))
+  
+  # write final modified data -----------------------------------------------------
+  
+  write_csv(df_final_cleaned_data, file = paste0("outputs/", butteR::date_file_prefix(), "_clean_data_", input_tool_data_name, ".csv"))
+  
+}
