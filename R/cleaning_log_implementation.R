4839802844dd91d72e8657b42826c43eec0f37b2
Anthony Twesigye <twesigye10@gmail.com>
2021-11-17 09:27:19 +0300

    reading dap from google sheets

--- R/cleaning_log_implementation.R
+++ R/cleaning_log_implementation.R
@@ -1,52 +1,18 @@
 library(tidyverse)
 library(lubridate)
 library(glue)
-source("R/cleaning_log_function.R")
+library(googlesheets4)
+source("R/data_analysis_function.R")
 
-# quantitative data -------------------------------------------------------
+# process dap -------------------------------------------------------
 
-cleaning_log(input_log_name = "PA_KI_questionnaire_September_2021_combined_checks",
-             input_tool_data_name = "PA_KI_questionnaire_September_2021",
-             input_tool_name = "PA_KI_questionnaire_Tool_2021")
+df_host_dap <- read_sheet(ss = "1Q-kOf4SppwKRWVvYJ4PJNzRJmFwRsM_S625bkbut7tw", sheet = "DAP_host")
 
-cleaning_log(input_log_name = "PA_KI_questionnaire_October_2021_Kampala_combined_checks",
-             input_tool_data_name = "PA_KI_questionnaire_October_2021_Kampala",
-             input_tool_name = "PA_KI_questionnaire_Tool_2021_Kampala")
+df_key_stakeholders <- read_sheet(ss = "1Q-kOf4SppwKRWVvYJ4PJNzRJmFwRsM_S625bkbut7tw", sheet = "DAP_key_stakeholders")
 
-# qualitative data -------------------------------------------------------
+df_minority_dap <- read_sheet(ss = "1Q-kOf4SppwKRWVvYJ4PJNzRJmFwRsM_S625bkbut7tw", sheet = "DAP_minority")
 
-# host_community ----------------------------------------------------------
-cleaning_log(input_log_name = "PA_KI_host_community_questionnaire_October_2021_combined_checks",
-             input_tool_data_name = "PA_KI_host_community_questionnaire_October_2021",
-             input_tool_name = "PA_KI_host_community_Tool_2021")
+df_others_dap <- read_sheet(ss = "1Q-kOf4SppwKRWVvYJ4PJNzRJmFwRsM_S625bkbut7tw", sheet = "DAP_others")
 
-cleaning_log(input_log_name = "PA_KI_host_community_questionnaire_October_2021_Kampala_combined_checks",
-             input_tool_data_name = "PA_KI_host_community_questionnaire_October_2021_Kampala",
-             input_tool_name = "PA_KI_host_community_Tool_2021_Kampala")
+df_phonesurvey_dap <- read_sheet(ss = "1Q-kOf4SppwKRWVvYJ4PJNzRJmFwRsM_S625bkbut7tw", sheet = "DAP_phone_surveys")
 
-# key_stakeholders --------------------------------------------------------
-cleaning_log(input_log_name = "PA_KI_key_stakeholders_questionnaire_October_2021_combined_checks",
-             input_tool_data_name = "PA_KI_key_stakeholders_questionnaire_October_2021",
-             input_tool_name = "PA_KI_key_stakeholders_Tool_2021")
-
-cleaning_log(input_log_name = "PA_KI_key_stakeholders_questionnaire_October_2021_Kampala_combined_checks",
-             input_tool_data_name = "PA_KI_key_stakeholders_questionnaire_October_2021_Kampala",
-             input_tool_name = "PA_KI_key_stakeholders_Tool_2021_Kampala")
-
-# specific_groups --------------------------------------------------------
-cleaning_log(input_log_name = "PA_KI_specific_groups_questionnaire_October_2021_combined_checks",
-             input_tool_data_name = "PA_KI_specific_groups_questionnaire_October_2021",
-             input_tool_name = "PA_KI_specific_groups_Tool_2021")
-
-cleaning_log(input_log_name = "PA_KI_specific_groups_questionnaire_October_2021_Kampala_combined_checks",
-             input_tool_data_name = "PA_KI_specific_groups_questionnaire_October_2021_Kampala",
-             input_tool_name = "PA_KI_specific_groups_Tool_2021_Kampala")
-
-# other_minorities --------------------------------------------------------
-cleaning_log(input_log_name = "PA_KI_other_minorities_questionnaire_October_2021_combined_checks",
-             input_tool_data_name = "PA_KI_other_minorities_questionnaire_October_2021",
-             input_tool_name = "PA_KI_other_minorities_Tool_2021")
-
-cleaning_log(input_log_name = "PA_KI_other_minorities_questionnaire_October_2021_Kampala_combined_checks",
-             input_tool_data_name = "PA_KI_other_minorities_questionnaire_October_2021_Kampala",
-             input_tool_name = "PA_KI_other_minorities_Tool_2021_Kampala")
