8cde2b10481748a76886da408118e53cbde919cc
Anthony Twesigye <twesigye10@gmail.com>
2021-11-18 15:34:13 +0300

    removing the code for the dap

--- R/data_analysis_implementation.R
+++ R/data_analysis_implementation.R
@@ -5,28 +5,6 @@ library(glue)
 
 source("R/data_analysis_function.R")
 
-# process dap -------------------------------------------------------
-
-df_host_dap <- read_sheet(ss = "1Q-kOf4SppwKRWVvYJ4PJNzRJmFwRsM_S625bkbut7tw", sheet = "DAP_host")
-
-df_key_stakeholders <- read_sheet(ss = "1Q-kOf4SppwKRWVvYJ4PJNzRJmFwRsM_S625bkbut7tw", sheet = "DAP_key_stakeholders")
-
-df_minority_dap <- read_sheet(ss = "1Q-kOf4SppwKRWVvYJ4PJNzRJmFwRsM_S625bkbut7tw", sheet = "DAP_minority")
-
-df_others_dap <- read_sheet(ss = "1Q-kOf4SppwKRWVvYJ4PJNzRJmFwRsM_S625bkbut7tw", sheet = "DAP_others")
-
-df_phonesurvey_dap <- read_sheet(ss = "1Q-kOf4SppwKRWVvYJ4PJNzRJmFwRsM_S625bkbut7tw", sheet = "DAP_phone_surveys")
-
-
-
-# check indicators --------------------------------------------------------
-
-readxl::read_excel(path = "inputs/PA_KI_host_community_questionnaire_October_2021.xlsx") %>% 
-  select(contains("/")) %>% 
-  colnames() %>% 
-  str_replace(pattern = "/\\w+", replacement = "") %>% 
-  unique()
-
 # quantitative data -------------------------------------------------------
 data_analysis(input_clean_data_name = "PA_KI_questionnaire_September_2021_combined_checks",
              input_dap_name = "PA_KI_questionnaire_September_2021",
