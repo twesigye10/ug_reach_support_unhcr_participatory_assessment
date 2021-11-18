7a07b43141454ef411c461f8a9298827de46f701
Anthony Twesigye <twesigye10@gmail.com>
2021-11-18 14:23:26 +0300

    handling PII

--- R/cleaning_log_implementation.R
+++ R/cleaning_log_implementation.R
@@ -3,50 +3,62 @@ library(lubridate)
 library(glue)
 source("R/cleaning_log_function.R")
 
+vars_to_remove <- c("contact_information_function",	"contact_information_gender",	"contact_information_location",	"contact_information_enum_org",	"contact_information__enum_function")
+
 # quantitative data -------------------------------------------------------
 
 cleaning_log(input_log_name = "PA_KI_questionnaire_September_2021_combined_checks",
              input_tool_data_name = "PA_KI_questionnaire_September_2021",
-             input_tool_name = "PA_KI_questionnaire_Tool_2021")
+             input_tool_name = "PA_KI_questionnaire_Tool_2021", 
+             input_vars_to_remove_from_data = vars_to_remove)
 
 cleaning_log(input_log_name = "PA_KI_questionnaire_October_2021_Kampala_combined_checks",
              input_tool_data_name = "PA_KI_questionnaire_October_2021_Kampala",
-             input_tool_name = "PA_KI_questionnaire_Tool_2021_Kampala")
+             input_tool_name = "PA_KI_questionnaire_Tool_2021_Kampala", 
+             input_vars_to_remove_from_data = vars_to_remove)
 
 # qualitative data -------------------------------------------------------
 
 # host_community ----------------------------------------------------------
 cleaning_log(input_log_name = "PA_KI_host_community_questionnaire_October_2021_combined_checks",
              input_tool_data_name = "PA_KI_host_community_questionnaire_October_2021",
-             input_tool_name = "PA_KI_host_community_Tool_2021")
+             input_tool_name = "PA_KI_host_community_Tool_2021", 
+             input_vars_to_remove_from_data = vars_to_remove)
 
 cleaning_log(input_log_name = "PA_KI_host_community_questionnaire_October_2021_Kampala_combined_checks",
              input_tool_data_name = "PA_KI_host_community_questionnaire_October_2021_Kampala",
-             input_tool_name = "PA_KI_host_community_Tool_2021_Kampala")
+             input_tool_name = "PA_KI_host_community_Tool_2021_Kampala", 
+             input_vars_to_remove_from_data = vars_to_remove)
 
 # key_stakeholders --------------------------------------------------------
 cleaning_log(input_log_name = "PA_KI_key_stakeholders_questionnaire_October_2021_combined_checks",
              input_tool_data_name = "PA_KI_key_stakeholders_questionnaire_October_2021",
-             input_tool_name = "PA_KI_key_stakeholders_Tool_2021")
+             input_tool_name = "PA_KI_key_stakeholders_Tool_2021", 
+             input_vars_to_remove_from_data = vars_to_remove)
 
 cleaning_log(input_log_name = "PA_KI_key_stakeholders_questionnaire_October_2021_Kampala_combined_checks",
              input_tool_data_name = "PA_KI_key_stakeholders_questionnaire_October_2021_Kampala",
-             input_tool_name = "PA_KI_key_stakeholders_Tool_2021_Kampala")
+             input_tool_name = "PA_KI_key_stakeholders_Tool_2021_Kampala", 
+             input_vars_to_remove_from_data = vars_to_remove)
 
 # specific_groups --------------------------------------------------------
 cleaning_log(input_log_name = "PA_KI_specific_groups_questionnaire_October_2021_combined_checks",
              input_tool_data_name = "PA_KI_specific_groups_questionnaire_October_2021",
-             input_tool_name = "PA_KI_specific_groups_Tool_2021")
+             input_tool_name = "PA_KI_specific_groups_Tool_2021", 
+             input_vars_to_remove_from_data = vars_to_remove)
 
 cleaning_log(input_log_name = "PA_KI_specific_groups_questionnaire_October_2021_Kampala_combined_checks",
              input_tool_data_name = "PA_KI_specific_groups_questionnaire_October_2021_Kampala",
-             input_tool_name = "PA_KI_specific_groups_Tool_2021_Kampala")
+             input_tool_name = "PA_KI_specific_groups_Tool_2021_Kampala", 
+             input_vars_to_remove_from_data = vars_to_remove)
 
 # other_minorities --------------------------------------------------------
 cleaning_log(input_log_name = "PA_KI_other_minorities_questionnaire_October_2021_combined_checks",
              input_tool_data_name = "PA_KI_other_minorities_questionnaire_October_2021",
-             input_tool_name = "PA_KI_other_minorities_Tool_2021")
+             input_tool_name = "PA_KI_other_minorities_Tool_2021", 
+             input_vars_to_remove_from_data = vars_to_remove)
 
 cleaning_log(input_log_name = "PA_KI_other_minorities_questionnaire_October_2021_Kampala_combined_checks",
              input_tool_data_name = "PA_KI_other_minorities_questionnaire_October_2021_Kampala",
-             input_tool_name = "PA_KI_other_minorities_Tool_2021_Kampala")
+             input_tool_name = "PA_KI_other_minorities_Tool_2021_Kampala", 
+             input_vars_to_remove_from_data = vars_to_remove)
