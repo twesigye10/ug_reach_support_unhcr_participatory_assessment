9a698fdb14022a974411c0775bdcd261dcdc2c8c
Anthony Twesigye <twesigye10@gmail.com>
2021-11-19 14:32:08 +0300

    removed code for testing average time of all quantitative surveys

--- R/checks_for_data_collection.R
+++ R/checks_for_data_collection.R
@@ -16,21 +16,6 @@ check_quantitative_tool_data(input_tool_data_name = "PA_KI_questionnaire_Septemb
 check_quantitative_tool_data(input_tool_data_name = "PA_KI_questionnaire_October_2021_Kampala",
                              input_tool_name = "PA_KI_questionnaire_Tool_2021_Kampala")
 
-# average survey_time
-df_questionnaire <- readxl::read_excel(path = "inputs/PA_KI_questionnaire_September_2021.xlsx") %>% 
-  mutate(start = as_datetime(start),
-         end = as_datetime(end),
-         int.survey_time_interval = lubridate::time_length(end - start, unit = "min"),
-         int.survey_time_interval = ceiling(int.survey_time_interval))
-mean_time_questionnaire <- round(mean(df_questionnaire$int.survey_time_interval), 0)
-
-df_questionnaire_kla <- readxl::read_excel(path = "inputs/PA_KI_questionnaire_October_2021_Kampala.xlsx") %>% 
-  mutate(start = as_datetime(start),
-         end = as_datetime(end),
-         int.survey_time_interval = lubridate::time_length(end - start, unit = "min"),
-         int.survey_time_interval = ceiling(int.survey_time_interval))
-mean_time_questionnaire_kla <- round(mean(df_questionnaire_kla$int.survey_time_interval), 0)
-
 # qualitative data -------------------------------------------------------
 
 # host_community ----------------------------------------------------------
