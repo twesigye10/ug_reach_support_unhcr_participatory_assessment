7c309f64487cd857de1b18f1424276e72b42dd6e
Anthony Twesigye <twesigye10@gmail.com>
2021-11-19 19:41:31 +0300

    corrected clean data name and vars for split

--- R/data_analysis_implementation.R
+++ R/data_analysis_implementation.R
@@ -5,49 +5,52 @@ library(glue)
 
 source("R/support_functions.R")
 
-# quantitative data -------------------------------------------------------
-data_analysis(input_clean_data_name = "PA_KI_questionnaire_September_2021_combined_checks",
-             input_dap_name = "PA_KI_questionnaire_September_2021",
-             input_vars_for_split = vars_for_split)
+vars_for_split_general <- c("all", "only_settlements")
+vars_for_split_kampala <- c("all")
 
-data_analysis(input_clean_data_name = "PA_KI_questionnaire_October_2021_Kampala_combined_checks",
+# quantitative data -------------------------------------------------------
+data_analysis(input_clean_data_name = "clean_data_PA_KI_questionnaire_September_2021",
+             input_dap_name = "PA_KI_questionnaire_September_2021",
+             input_vars_for_split = vars_for_split_general)
+
+data_analysis(input_clean_data_name = "clean_data_PA_KI_questionnaire_October_2021_Kampala",
              input_dap_name = "PA_KI_questionnaire_October_2021_Kampala",
-             input_vars_for_split = vars_for_split)
+             input_vars_for_split = vars_for_split_kampala)
 
 # qualitative data -------------------------------------------------------
 
 # host_community ----------------------------------------------------------
-data_analysis(input_clean_data_name = "PA_KI_host_community_questionnaire_October_2021_combined_checks",
+data_analysis(input_clean_data_name = "clean_data_PA_KI_host_community_questionnaire_October_2021",
              input_dap_name = "PA_KI_host_community_questionnaire_October_2021",
-             input_vars_for_split = vars_for_split)
+             input_vars_for_split = vars_for_split_general)
 
-data_analysis(input_clean_data_name = "PA_KI_host_community_questionnaire_October_2021_Kampala_combined_checks",
+data_analysis(input_clean_data_name = "clean_data_PA_KI_host_community_questionnaire_October_2021_Kampala",
              input_dap_name = "PA_KI_host_community_questionnaire_October_2021_Kampala",
-             input_vars_for_split = vars_for_split)
+             input_vars_for_split = vars_for_split_kampala)
 
 # key_stakeholders --------------------------------------------------------
-data_analysis(input_clean_data_name = "PA_KI_key_stakeholders_questionnaire_October_2021_combined_checks",
+data_analysis(input_clean_data_name = "clean_data_PA_KI_key_stakeholders_questionnaire_October_2021",
              input_dap_name = "PA_KI_key_stakeholders_questionnaire_October_2021",
-             input_vars_for_split = vars_for_split)
+             input_vars_for_split = vars_for_split_general)
 
-data_analysis(input_clean_data_name = "PA_KI_key_stakeholders_questionnaire_October_2021_Kampala_combined_checks",
+data_analysis(input_clean_data_name = "clean_data_PA_KI_key_stakeholders_questionnaire_October_2021_Kampala",
              input_dap_name = "PA_KI_key_stakeholders_questionnaire_October_2021_Kampala",
-             input_vars_for_split = vars_for_split)
+             input_vars_for_split = vars_for_split_kampala)
 
 # specific_groups --------------------------------------------------------
-data_analysis(input_clean_data_name = "PA_KI_specific_groups_questionnaire_October_2021_combined_checks",
+data_analysis(input_clean_data_name = "clean_data_PA_KI_specific_groups_questionnaire_October_2021",
              input_dap_name = "PA_KI_specific_groups_questionnaire_October_2021",
-             input_vars_for_split = vars_for_split)
+             input_vars_for_split = vars_for_split_general)
 
-data_analysis(input_clean_data_name = "PA_KI_specific_groups_questionnaire_October_2021_Kampala_combined_checks",
+data_analysis(input_clean_data_name = "clean_data_PA_KI_specific_groups_questionnaire_October_2021_Kampala",
              input_dap_name = "PA_KI_specific_groups_questionnaire_October_2021_Kampala",
-             input_vars_for_split = vars_for_split)
+             input_vars_for_split = vars_for_split_kampala)
 
 # other_minorities --------------------------------------------------------
-data_analysis(input_clean_data_name = "PA_KI_other_minorities_questionnaire_October_2021_combined_checks",
+data_analysis(input_clean_data_name = "clean_data_PA_KI_other_minorities_questionnaire_October_2021",
              input_dap_name = "PA_KI_other_minorities_questionnaire_October_2021",
-             input_vars_for_split = vars_for_split)
+             input_vars_for_split = vars_for_split_general)
 
-data_analysis(input_clean_data_name = "PA_KI_other_minorities_questionnaire_October_2021_Kampala_combined_checks",
+data_analysis(input_clean_data_name = "clean_data_PA_KI_other_minorities_questionnaire_October_2021_Kampala",
              input_dap_name = "PA_KI_other_minorities_questionnaire_October_2021_Kampala",
-             input_vars_for_split = vars_for_split)
+             input_vars_for_split = vars_for_split_kampala)
